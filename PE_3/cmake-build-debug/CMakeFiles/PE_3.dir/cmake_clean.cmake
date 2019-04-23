192ff5f33a9ce76520f8a740fc8debd119050ec3
mojitas <msd16007@student.mdh.se>
2019-04-23 14:03:00 +0200

    added comments

---
+++ PE_3/cmake-build-debug/CMakeFiles/PE_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PE_3.dir/main.c.o"
+  "PE_3.pdb"
+  "PE_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PE_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
