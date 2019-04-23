a5e6b6fff27551e5d8e238ccd2a1c97f993fc948
mojitas <msd16007@student.mdh.se>
2019-04-23 10:52:40 +0200

    added comments

---
+++ PE_2/cmake-build-debug/CMakeFiles/PE_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PE_2.dir/main.c.o"
+  "PE_2.pdb"
+  "PE_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PE_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
