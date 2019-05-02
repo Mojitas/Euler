68451acd1a0aac0de43b7da4c2bef25c98648108
mojitas <msd16007@student.mdh.se>
2019-05-03 01:47:39 +0200

    bit on the way

--- PE_4/cmake-build-debug/cmake_install.cmake
+++ PE_4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mojitas/CLionProjects/PE_4
+# Install script for directory: /home/mojitas/CLionProjects/Euler/PE_4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mojitas/CLionProjects/PE_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mojitas/CLionProjects/Euler/PE_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- PE_4/main.c
+++ PE_4/main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Euler project 4
+/*
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+int isPal(int number){
+
+    char* num = (char*)malloc(8);
+    int length = strlen(num), modCut=10;
+
+    for (int j = 0; j <length ; j++) {
+        char[j] = itoa(number%modCut);
+        modCut=modCut*10;
+    }
+
+    puts(num);
+    exit(EXIT_SUCCESS);
+
+
+
+    for (int i = 0; i<length ; i) {
+
+
+        if(num[i]==num[length-i])
+            i++;
+
+        else
+            return 0;
+    }
+    return 1;
+}
+
+
+int numberGenerator(void){
+    int a=999,pal;
+    for (int i = 999; i>99 ; i--) {
+        pal=a*i;
+        if(isPal(pal))
+            return pal;
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int number;
+
+
+    //number = numberGenerator();
+    //printf("Biggest palindromic number constisting of the product of two 3-digit numbers: %d\n",number);
+
     return 0;
 }
